eccc68fdcc75b8f7193f01803d30d968a3f19ccd
zozopak <zozo.pakdaman@yahoo.com>
2021-01-14 18:51:15 +0330

    add swagger to webservices

--- marketplace-service/src/main/java/com/mapsa/marketplace/MarketplaceServiceApplication.java
+++ marketplace-service/src/main/java/com/mapsa/marketplace/MarketplaceServiceApplication.java
@@ -6,11 +6,26 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableSwagger2
 public class MarketplaceServiceApplication {
     // TODO: 11/27/2020  @ResponseStatus/Database Tables & relations/spring validations
 
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.mapsa.marketplace")).build();
+    }
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
 
 }
